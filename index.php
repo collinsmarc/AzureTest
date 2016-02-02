53de56fd6ec10a977cd9b5d96fe1d89013e81f72
unknown <1510646@RG41836.rgu.ac.uk>
2016-02-02 08:57:35 +0000

    Initial commit

---
+++ index.php
@@ -0,0 +1,22 @@
+<?php
+
+/**
+ * Created by PhpStorm.
+ * User: 1510646
+ * Date: 02/02/2016
+ * Time: 08:55
+ */
+class index
+{
+<!DOCTYPE html>
+<html>
+<head lang="en">
+<meta charset="UTF-8">
+<title>Deployment Test</title>
+</head>
+<body>
+<H1> It's ALIVE!!!</H1>
+</body>
+</html>
+
+}
