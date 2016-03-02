d3975ddd174936f7e99e17cd45686cdbf9748fc4
collinsmarc <m.a.collins@rgu.ac.uk>
2016-03-02 12:46:41 +0000

    rgu

--- GroupWork/Group Work/test.php
+++ GroupWork/Group Work/test.php
@@ -1,20 +1,21 @@
 <?php
 error_reporting(-1);
+if(isset($_POST['submit'])) {
 
-
-$dsn = "mysql:host=eu-cdbr-azure-north-d.cloudapp.net;dbname=db1510646_gameshare";
-$username = "b52b6c6935c6d2";
-$password = "26ebeed0";
-try {
-    $conn = new PDO( $dsn, $username, $password );
-    $conn->setAttribute( PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION );
-} catch ( PDOException $e ) {
-    echo "Connection failed: " . $e->getMessage();}
+    $dsn = "mysql:host=eu-cdbr-azure-north-d.cloudapp.net;dbname=db1510646_gameshare";
+    $username = "b52b6c6935c6d2";
+    $password = "26ebeed0";
+    try {
+        $conn = new PDO($dsn, $username, $password);
+        $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    } catch (PDOException $e) {
+        echo "Connection failed: " . $e->getMessage();
+    }
 
     $query = "";
     $title = $_POST['title'];
 
-    if(isset($title)) {//if keyword set goes here
+    if (isset($title)) {//if keyword set goes here
         $query = "SELECT * FROM gameCollection WHERE Title LIKE '%$title%'";
         try {
             $results = $conn->query($sql);
@@ -38,6 +39,7 @@ try {
         }
         $conn = null;
     }
+}
 ?>
 
 
