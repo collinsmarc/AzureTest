0b9bce977a6be5b689dab26eefa7e68b195ed590
collinsmarc <m.a.collins@rgu.ac.uk>
2016-03-07 09:57:54 +0000

    o

---
+++ Week6/array.php
@@ -0,0 +1,17 @@
+<!doctype html>
+<html>
+<head>
+</head>
+<body>
+<p>
+    <?php
+   $provisionedActivites=array("specs","drugs","rock and roll");
+
+    foreach($provisionedActivites as $x){
+        print "<p>$x</p>";
+    }
+
+    ?>
+</p>
+</body>
+</html>
