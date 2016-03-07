59f68a24a0acd8afda7f6f08e7361ce92472dfd7
collinsmarc <m.a.collins@rgu.ac.uk>
2016-03-07 10:11:50 +0000

    o

--- Week6/specs4.php
+++ Week6/specs4.php
@@ -6,8 +6,10 @@
 <p>
     <?php
     for ($i=1;$i<30;$i++){
-        print "on the".$i."of the month no products ae available"+
-            "on the " .$i."of the month specs are available";
+        echo "on the".$i."of the month no products ae available <br>"+
+            "on the " .$i."of the month specs are available <br>"+
+            "On the".$i."of the month mugs are available <br>"+
+            "On the" .$i."of the month specs and sausage rolls are available";
 
 
     }
