ba13b7eba0de5a41c7860edcd1fca9520f880617
collinsmarc <m.a.collins@rgu.ac.uk>
2016-03-07 10:09:23 +0000

    o

---
+++ Week6/specs4.php
@@ -0,0 +1,19 @@
+<!doctype html>
+<html>
+<head>
+</head>
+<body>
+<p>
+    <?php
+    for ($i=1;$i<30;$i++){
+        print "on the".$i."of the month no products ae available"+
+            "on the " .$i."of the month specs are available";
+
+
+    }
+
+
+    ?>
+</p>
+</body>
+</html>
