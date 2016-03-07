40a97d7158ac6a7c345929e6002ce95be59af7db
collinsmarc <m.a.collins@rgu.ac.uk>
2016-03-07 09:34:32 +0000

    o

---
+++ Week6/index.php
@@ -0,0 +1,15 @@
+<!doctype html>
+<html>
+<head>
+</head>
+<body>
+<p>
+    <?php
+   $myname="marc";
+    $myage=25;
+    echo "my name is" .$myname."and i am".$myage;
+
+    ?>
+</p>
+</body>
+</html>
