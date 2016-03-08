aa8793ce7852217fbaddef969f6b66ffc8b742b9
collinsmarc <m.a.collins@rgu.ac.uk>
2016-03-08 09:24:35 +0000

    o

---
+++ Week6/arrayFunctions.php
@@ -0,0 +1,20 @@
+<!doctype html>
+<html>
+<head>
+</head>
+<body>
+<p>
+    <?php
+    $favbands=array();
+    array_push($favbands,"smashing pumpkins");
+    array_push($favbands,"pearl jam");
+    array_push($favbands,"U2");
+
+    print count($favbands);
+
+    print "<br>.$favbands[1]";
+
+    ?>
+</p>
+</body>
+</html>
