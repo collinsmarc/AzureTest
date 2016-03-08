3627f8d9b1e51a4b5d5efe0e1b5832e31093af2e
collinsmarc <m.a.collins@rgu.ac.uk>
2016-03-08 09:50:18 +0000

    o

---
+++ Week6/ownfunctions.php
@@ -0,0 +1,18 @@
+<!doctype html>
+<html>
+<head>
+</head>
+<body>
+<p>
+    <?php
+    function square($number){
+        echo $number*$number;
+    }
+
+    $n=25;
+    square($n)
+
+    ?>
+</p>
+</body>
+</html>
