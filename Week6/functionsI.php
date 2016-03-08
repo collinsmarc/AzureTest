a974f74acb5793c2b6242a95c8460bb7352bcd19
collinsmarc <m.a.collins@rgu.ac.uk>
2016-03-08 09:10:56 +0000

    o

---
+++ Week6/functionsI.php
@@ -0,0 +1,16 @@
+<!doctype html>
+<html>
+<head>
+</head>
+<body>
+<p>
+    <?php
+    $myname="Marc Collins";
+    $length=strlen($myname);
+
+    $partial=substr($myname,0,2);
+
+    ?>
+</p>
+</body>
+</html>
