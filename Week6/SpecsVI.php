3b5f551453e9f1a1dfb37a8eaba9d74f8862a830
collinsmarc <m.a.collins@rgu.ac.uk>
2016-03-08 09:46:09 +0000

    o

--- Week6/SpecsVI.php
+++ Week6/SpecsVI.php
@@ -29,7 +29,15 @@
     $WINNER=($favpeeps[$randnum]);
 
     print "The winner of the Specs is ".strtoupper($WINNER);
+    unset ($favpeeps[$WINNER]);
+
+    $randnum=rand(0,count($favpeeps));
+    $WINNER=($favpeeps[$randnum]);
     print "<br> The Winner of the Sausage rolls is  ".strtoupper($WINNER);
+    unset ($favpeeps[$WINNER]);
+
+    $randnum=rand(0,count($favpeeps));
+    $WINNER=($favpeeps[$randnum]);
     print "<br> The winner of the mugs is ".strtoupper($WINNER);
 
 
