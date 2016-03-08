4e02e4e0c086522cd908ef95511c2191dc69fecd
collinsmarc <m.a.collins@rgu.ac.uk>
2016-03-08 09:59:56 +0000

    o

--- Week6/ownfunctions.php
+++ Week6/ownfunctions.php
@@ -5,13 +5,21 @@
 <body>
 <p>
     <?php
-    function square($number){
-        echo $number*$number;
+    function wantedPerson($name,$noSpecs,$noMugs,$noSausageRolls){
+
+        $reward=10*(($noSpecs*$noMugs*$noSausageRolls)*($noSpecs*$noMugs*$noSausageRolls))/2;
+
+        echo "Wanted :".$name
+            ."<br> Known to be in possesion of the following items: "
+            ."<br> Specs ".$noSpecs
+            ."<br> Mugs ".$noMugs
+            ."<br> Sausage Rolls ".$noSausageRolls
+            ."<br> Award for capture: ".$reward;
     }
 
-    $n=25;
-    square($n)
-
+    wantedPerson("Marc",2,3,4);
+    wantedPerson("David",10,20,40);
+    wantedPerson("Petey",1,1,1);
     ?>
 </p>
 </body>
