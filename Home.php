e4b81974c10b33e4899bd879116aab479d229fcd
collinsmarc <m.a.collins@rgu.ac.uk>
2016-03-14 10:20:35 +0000

    Added files via upload

---
+++ Home.php
@@ -0,0 +1,12 @@
+<html>
+<head>
+    <meta charset="utf-8">
+    <title> PHP Login Form without Session</title>
+    <link rel="stylesheet" href="style.css" type="text/css"/>
+</head>
+
+<body>
+<h1>Hello</h1>
+</body>
+
+</html>
---
+++ login.php
@@ -0,0 +1,25 @@
+<?php
+include ("connection.php"); //establish connection to DB
+
+if (empty($_POST['username']) || empty ($_POST['password'])){
+    echo "Both fields are required";
+
+}else{
+    $username=$_POST['username'];
+    $password=$_POST['password'];
+}
+
+$sql="SELECT uid FROM users WHERE username='$username' and password='$password'";
+
+$result=mysqli_query($db,$sql);
+
+
+
+if (mysqli_num_rows($result)==1){
+    header("location:home.php");//redirect to home page
+
+}else{
+    echo "Incorrect username of password";
+}
+
+?>
