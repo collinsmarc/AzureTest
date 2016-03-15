9cedec8993b07fd021adabf7eaa339386b30d44b
collinsmarc <m.a.collins@rgu.ac.uk>
2016-03-15 08:55:31 +0000

    hh

---
+++ Week7-login/Home.php
@@ -0,0 +1,21 @@
+
+
+<html>
+<head>
+    <meta charset="utf-8">
+    <title> PHP Login Form without Session</title>
+    <link rel="stylesheet" href="style.css" type="text/css"/>
+</head>
+
+<body>
+<h1>Hello <?php
+    echo ($_GET[$username]); ?>
+
+
+
+
+
+   </h1>
+</body>
+
+</html>
