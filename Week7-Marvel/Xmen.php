1bcb20c356e6ddda1e484237c9fcc224be773d91
collinsmarc <m.a.collins@rgu.ac.uk>
2016-03-15 10:00:54 +0000

    hh

--- Week7-Marvel/Xmen.php
+++ Week7-Marvel/Xmen.php
@@ -1,2 +1,63 @@
 <?php
 include("connection.php"); //establish connection to DB
+?>
+<!doctype html>
+<html>
+<head>
+    <meta charset="utf-8">
+    <title> MARVEL SEARCH</title>
+    <link rel="stylesheet" href="StyleMarvel.css.css" type="text/css"/>
+    <link rel="stylesheet"
+          href="unsemantic-grid-responsive-tablet.css">
+    <meta name="viewport" content="width=device-width,initial-scale=1,minimum-scale=1,
+maximum-scale=1"/>
+</head>
+
+<header>
+    <h1>MARVEL SEARCHBD
+        <h1>
+            <h2>Here to meet all your searching needs</h2>
+            <nav id="quicklinks">
+                <ul>
+
+                    <li><a href="index.php">Home Page</a></li>
+                    <li><a href="allMovies.php">All Movies</a></li>
+                    <li><a href="allMarvel.php">All Marvel Movies</a></li>
+                    <li><a href="2010After.php">After 2010</a></li>
+                    <li><a href="Xmen.php">All X-Men </a></li>
+                </ul>
+</header>
+
+<main>
+
+
+
+    <?php
+    $sql = "SELECT * FROM marvelmovies WHERE title LIKE '%X-Men%'";
+    $result = mysqli_query($db, $sql);
+
+    while ($row = $result->fetch_array()) {
+        $movieTitle = $row['title'];
+        print "<table id='results'>\n";
+        echo "<th>MarvelID</th><th>Year</th><th>Title</th><th>Studio</th><th id='desc'>Notes</th>";
+        foreach ($result as $row) {
+            echo "<tr>";
+            echo "<td>" . $row["marvelMovieID"] . "</td>";
+            echo "<td>" . $row["yearReleased"] . "</td>";
+            echo "<td>" . $row["title"] . "</td>";
+            echo "<td>" . $row["productionStudio"] . "</td>";
+            echo "<td>" . $row["notes"] . "</td>";
+
+        }
+    }
+
+
+    ?>
+
+
+</main>
+
+
+<footer>
+    <p>all rights reserved to the MMDB. Admin Marc</p>
+</footer>
