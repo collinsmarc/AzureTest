6d8f50b7a95f12fc2b9b09bc61de9a89fd7e0571
collinsmarc <m.a.collins@rgu.ac.uk>
2016-03-17 13:05:07 +0000

    hh

--- Group1703/Searchbroken.php
+++ Group1703/Searchbroken.php
@@ -13,7 +13,7 @@
     <div id="search">
         <Form Name ="login" action="login.php" method="post">
             <div id="logintext">
-        <?/* if(!isset($_SESSION['username'])){ echo
+        /* if(!isset($_SESSION['username'])){ echo
             '<font size="-2"><label for="username">Username :</label><input id="username" name="username" type="text" size="-2"/><label for="Password">Password :</label><input id="password" name="password" type="password" size="-2"/></font><input class="form-submit" type="submit" value="Login" />
              <a id="register" href="Register.html">Not a member? Register.</a>
               ';}
@@ -21,7 +21,7 @@
 else{       echo "Logged in as: ".$_SESSION['username'];
             echo "<input id='logoutButton' type='submit' type='submit' value='Log Out'>";
 
-}*/?>
+}*/
             </div>
         </form>
     </div>-->
