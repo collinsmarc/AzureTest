bfd2aaf70d3d02d319f5f1dbf999fad1ebb56d88
collinsmarc <m.a.collins@rgu.ac.uk>
2016-03-17 11:29:54 +0000

    hh

--- Group1703/memberSite.php
+++ Group1703/memberSite.php
@@ -1,4 +1,5 @@
 <?php session_start(); ?>
+
 <!DOCTYPE html>
 
 
@@ -16,15 +17,36 @@
         <div id="logo">
             <h1><strong><font size="20">GameShare RGU</font></strong></h1>
         </div>
+        <form name="logout" action="logout.php" method="post">
+            <input type="submit" type="submit" value="logout">
+        </form>
 
 
 
-<main>
+
+
+
+<div id="menu">
+    <form action="results.php" method="post">
+        <ul>
+            <li><a href="index.html">Homepage</a></li>
+            <li><a href="#"></a></li>
+            <li><a href="#"></a></li>
+            <li><a href="search.html">Search</a></li>
+            <li><input id="qsearch" name="qsearch" type="text" placeholder="I want to borrow..."/><input id="qsgo"
+                                                                                                         type="submit"
+                                                                                                         value="Go">
+            </li>
+        </ul>
+    </form>
+    <br class="clearfix"/>
+</div>
+</div>
+</div>
+</body>
 
 
 
 
-<form name="logout" action="logout.php" method="post">
-    <input type="submit" type="submit" value="logout">
-</form>
-</main>
+
+        </main>
