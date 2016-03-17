6ac19a644a408bc7a1c4b9e6adb3e9a33ab39599
collinsmarc <m.a.collins@rgu.ac.uk>
2016-03-17 13:41:50 +0000

    hh

--- Group1703/Search.php
+++ Group1703/Search.php
@@ -12,14 +12,13 @@
             <h1><strong><font size="20">GameShare RGU</font></strong></h1>
         </div>
         <div id="search">
-
+            <Form Name ="login" action="login.php" method="post">
 
 
                <? if(!isset($_SESSION['username'])){
-               echo'<Form Name ="login" action="login.php" method="post">
+               echo'
                     <font size="-2"><label for="username">Username :</label><input id="username" name="username" type="text" size="-2"/><label for="Password">Password :</label><input id="password" name="password" type="password" size="-2"/></font><input class="form-submit" type="submit" value="Login" />
                     <a id="register" href="Register.html">Not a member? Register.</a>
-                    </form>
                ';}
                else{
                    echo "Logged in as: ".$_SESSION['username'];
@@ -32,7 +31,7 @@
 
 
                ?>
-
+            </form>
         </div>
         <div id="menu">
             <form action="results.php" method="post">
