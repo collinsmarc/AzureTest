8bb54a445128c52b1c82f097507cd66d0b642ced
collinsmarc <m.a.collins@rgu.ac.uk>
2016-03-21 10:17:32 +0000

    hh

--- Week8-GETPOST/insertsuperhero.php
+++ Week8-GETPOST/insertsuperhero.php
@@ -3,15 +3,15 @@ include("connection.php"); //establish connection to DB
 
 //insert into superhero db
 
-$firstname=$_POST["firstName"];
+$firstName=$_POST["firstName"];
 $lastName=$_POST["lastName"];
 $power=$_POST["power"];
 
 
 
 
-$sql="INSERT INTO superheroes ('firstName','lastName','mainSuperpower')
-VALUES ($firstname,$lastName,$power) ";
+$sql="INSERT INTO superheroes (firstName,lastName,mainSuperpower)
+VALUES ('$firstName','$lastName','$power') ";
 
 $result=$db->query($sql);
 
