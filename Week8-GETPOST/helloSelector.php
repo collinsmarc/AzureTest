a25c97aac0f777fbff54103e564fd8eb6f350071
collinsmarc <m.a.collins@rgu.ac.uk>
2016-03-21 09:38:46 +0000

    hh

--- Week8-GETPOST/helloSelector.php
+++ Week8-GETPOST/helloSelector.php
@@ -4,17 +4,14 @@
     <meta charset="utf-8">
     <title> Planets</title>
     </head>
-
-<a href="http://marcwebs.azurewebsites.net/Week8-GETPOST/helloPrinter.php?planet=Earth"></a>
-<a href="http://marcwebs.azurewebsites.net/Week8-GETPOST/helloPrinter.php?planet=Mars"></a>
-<a href="http://marcwebs.azurewebsites.net/Week8-GETPOST/helloPrinter.php?planet=Uranus"></a>
-
 <p>Where would you like to say hello?</p>
+<a href="http://marcwebs.azurewebsites.net/Week8-GETPOST/helloPrinter.php?planet=Earth">Earth</a>
+<a href="http://marcwebs.azurewebsites.net/Week8-GETPOST/helloPrinter.php?planet=Mars">mars</a>
+<a href="http://marcwebs.azurewebsites.net/Week8-GETPOST/helloPrinter.php?planet=Uranus">Uranus</a>
 
-<?php
- $planet=$_GET["planet"];
-echo "$planet <br>";
-?>
+
+
+
 
 
 </html>
