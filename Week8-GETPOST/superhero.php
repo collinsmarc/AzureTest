216226f453a1fceb8222280a2e2a0363f6b9cc6d
collinsmarc <m.a.collins@rgu.ac.uk>
2016-03-21 09:47:23 +0000

    hh

--- Week8-GETPOST/superhero.php
+++ Week8-GETPOST/superhero.php
@@ -10,6 +10,7 @@
         <label>Full Name</label><input type="text" name="fullname">
         <label>Gender (Male or Female)</label><input type="text" name="gender">
         <label>Main Power?</label> <input type="text" name="power">
+        <input type="submit" value="Submit">
     </form>
 
 
