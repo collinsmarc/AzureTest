ef66c9fc432d86de3fbd6db26fc4ff3f0caa388c
collinsmarc <m.a.collins@rgu.ac.uk>
2016-03-21 09:46:11 +0000

    hh

---
+++ Week8-GETPOST/superhero.php
@@ -0,0 +1,17 @@
+<!doctype html>
+<html>
+<head>
+    <meta charset="utf-8">
+    <title> Planets</title>
+</head>
+
+<main>
+    <form action="superheroResponse.php" method="post">
+        <label>Full Name</label><input type="text" name="fullname">
+        <label>Gender (Male or Female)</label><input type="text" name="gender">
+        <label>Main Power?</label> <input type="text" name="power">
+    </form>
+
+
+
+</main>
---
+++ Week8-GETPOST/superheroResponse.php
@@ -0,0 +1,9 @@
+<?php
+
+$name=$_POST["fullname"];
+$gender=$_POST["gender"];
+$power=$_POST["power"];
+
+echo $fullname.$gender.$power;
+
+?>
