3f3398fda309531ebbb4ad38c447f2f91f470969
collinsmarc <m.a.collins@rgu.ac.uk>
2016-03-21 10:20:21 +0000

    hh

--- Week8-GETPOST/connection.php
+++ Week8-GETPOST/connection.php
@@ -2,7 +2,7 @@
 define ('DB_SERVER', 'ap-cdbr-azure-east-c.cloudapp.net');
 define ('DB_USERNAME','bd567b6ba42458');
 define('DB_PASSWORD', '1b99eb9a');
-define('DB_DATABASE', 'webDev');
+define('DB_DATABASE', 'webdev');
 
 $db=mysqli_connect(DB_SERVER,DB_USERNAME,DB_PASSWORD,DB_DATABASE);
 
