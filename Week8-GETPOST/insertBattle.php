528862b23174e56714e12258f02cb5f368d4d73b
collinsmarc <m.a.collins@rgu.ac.uk>
2016-03-22 09:48:26 +0000

    hh

--- Week8-GETPOST/insertBattle.php
+++ Week8-GETPOST/insertBattle.php
@@ -1,1 +1,21 @@
 <?php
+include("connection.php"); //establish connection to DB
+
+//insert into battle db
+
+$superheroID=$_POST["superhero"];
+$villan=$_POST["villan"];
+
+
+
+
+$sql="INSERT INTO battles (superheroID,villanFought)
+VALUES ('$superheroID','$villan')";
+
+if (mysqli_query($db, $sql)) {
+} else {
+    echo "Error: " . $sql . "<br>" . mysqli_error($db);
+}
+
+$conn->close();
+?>
