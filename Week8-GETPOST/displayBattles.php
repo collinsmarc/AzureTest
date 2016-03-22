ca2b2ed37d6c679d749e155eb5cf4ec622939d0c
collinsmarc <m.a.collins@rgu.ac.uk>
2016-03-22 10:05:07 +0000

    hh

---
+++ Week8-GETPOST/displayBattles.php
@@ -0,0 +1,46 @@
+<?php
+include("connection.php"); //establish connection to DB
+?>
+
+<!doctype html>
+<html>
+<head>
+    <meta charset="utf-8">
+    <title> Battle Display</title>
+</head>
+
+<header>
+    <h1>Battles Fought Thus Far</h1>
+
+    </header>
+
+<main>
+    <h2> Here are the Battles Fought</h2>
+    <?
+    $sql="SELECT * FROM superherobattles";
+    $result=$db->query($sql);
+
+    while($row=$result->fetch_array()){
+        echo "<tr>";
+            echo "<td>" . $row["firstname"] . "</td>";
+            echo "<td>" . $row["lastname"] . "</td>";
+            echo "<td>" . $row["mainSuperPower"] . "</td>";
+            echo "<td>" . $row["villanFought"] . "</td>";
+
+    }
+
+
+
+
+
+    ?>
+
+
+
+
+
+    </main>
+
+
+
+
