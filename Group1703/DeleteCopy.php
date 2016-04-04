31e94d7b19506467e566b2a88e4e43a1a94947de
collinsmarc <m.a.collins@rgu.ac.uk>
2016-04-04 16:51:56 +0100

    Added files via upload

---
+++ Group1703/DeleteCopy.php
@@ -0,0 +1,32 @@
+<?session_start();
+if(!isset($_SESSION['username'])) {
+    header("Location:index.html");
+
+
+    error_reporting(-1);
+
+    $dsn = "mysql:host=eu-cdbr-azure-north-d.cloudapp.net;dbname=db1510646_gameshare";
+    $username = "b52b6c6935c6d2";
+    $password = "26ebeed0";
+    try {
+        $conn = new PDO($dsn, $username, $password);
+        $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+        $id=$_SESSION['username'];
+        $game=$row['gameID'];
+
+
+
+        $sql = "DELETE FROM owns
+WHERE studentID='$id' AND gameID='$gameID'";
+
+        $conn->exec($sql);
+        header('Location: memberSite.php');
+
+    } catch (PDOException $e) {
+        echo "Connection failed: " . $e->getMessage();
+    }
+
+    $conn = null;
+
+}
+?>
