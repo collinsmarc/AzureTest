74a0e51517672fb5a9690a4444dff47196968fce
collinsmarc <m.a.collins@rgu.ac.uk>
2016-04-04 15:49:16 +0100

    Added files via upload

--- Group1703/memberSite.php
+++ Group1703/memberSite.php
@@ -90,15 +90,15 @@ if (!isset($_SESSION['username'])) {
 
 
                             foreach ($results as $row) {
-                                print "<table id='results'>\n";
-                                echo "<th>game ID</th><th>Condition</th>";
+                                /*print "<table id='results'>\n";
+                                echo "<th>game ID</th><th>Condition</th>";*/
                                 foreach ($results as $row) {
                                     echo "<tr>";
                                     echo "<td>" . $row["gameID"] . "</td>";
                                     echo "<td>" . $row["game_condition"] . "</td>";
 
                                 }
-                                print "</table>\n";
+//                                print "</table>\n";
 
 
                             }
