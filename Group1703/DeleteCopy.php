a3bcf19d9146fc6ea948a05d54913c8d53ca6f80
collinsmarc <m.a.collins@rgu.ac.uk>
2016-04-06 13:27:33 +0100

    hh

--- Group1703/DeleteCopy.php
+++ Group1703/DeleteCopy.php
@@ -1,3 +1,4 @@
+<<<<<<< Updated upstream
 <?session_start();
 if(!isset($_SESSION['username'])) {
     header("Location:index.html");
@@ -31,3 +32,38 @@ if(!isset($_SESSION['username'])) {
 }
 header('Location: memberSite.php');
 ?>
+=======
+<?session_start();
+if(!isset($_SESSION['username'])) {
+    header("Location:index.html");
+
+
+    error_reporting(-1);
+
+    $dsn = "mysql:host=eu-cdbr-azure-north-d.cloudapp.net;dbname=db1510646_gameshare";
+    $username = "b52b6c6935c6d2";
+    $password = "26ebeed0";
+    try {
+        $conn = new PDO($dsn, $username, $password);
+        $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+        $id=$_SESSION['username'];
+        $game=$row['gameID'];
+
+
+
+        $sql = "DELETE FROM owns
+        WHERE studentID LIKE '$id'";
+
+        $conn->exec($sql);
+       
+
+    } catch (PDOException $e) {
+        echo "Connection failed: " . $e->getMessage();
+    }
+
+    $conn = null;
+
+}
+header('Location: memberSite.php');
+?>
+>>>>>>> Stashed changes
