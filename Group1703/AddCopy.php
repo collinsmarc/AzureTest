383b36ce2682c91040e053ddcf505dd405f17c80
collinsmarc <m.a.collins@rgu.ac.uk>
2016-04-06 15:25:26 +0100

    hh

--- Group1703/AddCopy.php
+++ Group1703/AddCopy.php
@@ -18,7 +18,7 @@ try {    $conn = new PDO($dsn, $username, $password);
     $condition=$_POST['conditionGame'];
     $gameID=$_POST['Titles'];
 
-    $inner="(SELECT MAX(copyID) FROM (SELECT IF(copyID IS NULL, 0, copyID) copyID FROM owns WHERE gameID LIKE '$gameID' ) AS maximum)";
+    $inner="(SELECT MAX(copyID)+1 FROM (SELECT IF(copyID IS NULL, 0, copyID) copyID FROM owns WHERE gameID LIKE '$gameID' ) AS maximum)";
     $query = "INSERT INTO owns (gameID, studentID, copyID, game_condition) VALUES ('$gameID','$id',$inner,'$condition')";
 
 
