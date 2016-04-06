a3fb0c81aac684daf85304f4b72d3f1883ed8e13
collinsmarc <m.a.collins@rgu.ac.uk>
2016-04-06 14:23:43 +0100

    hh

--- Group1703/AddCopy.php
+++ Group1703/AddCopy.php
@@ -8,8 +8,7 @@ error_reporting(-1);
 $dsn = "mysql:host=eu-cdbr-azure-north-d.cloudapp.net;dbname=db1510646_gameshare";
 $username = "b52b6c6935c6d2";
 $password = "26ebeed0";
-try {
-    $conn = new PDO($dsn, $username, $password);
+try {    $conn = new PDO($dsn, $username, $password);
     $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 
     $sql = "";
@@ -17,7 +16,7 @@ try {
     $id=$_SESSION['username'];
     $condition=$_POST['conditionGame'];
 
-    $sql = "INSERT INTO owns (gameID, studentID, copyID, game_condition) VALUES ('46','$id','1','$condition')";
+    $sql = "INSERT INTO owns (gameID, studentID, copyID, game_condition) VALUES ('32','$id','1','$condition')";
 
 
     $conn->exec($sql);
