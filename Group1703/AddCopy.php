76d2b032220a5fcdc9a77504c06143d38ba6f0fd
collinsmarc <m.a.collins@rgu.ac.uk>
2016-04-08 12:19:49 +0100

    Added files via upload

--- Group1703/AddCopy.php
+++ Group1703/AddCopy.php
@@ -1,102 +1,28 @@
 <? session_start();
 if (!isset($_SESSION['username'])) {
-    header("Location:index.html");}?>
-
-    <!DOCTYPE html>
-
-    <head>
-        <link rel="stylesheet" type="text/css" href="design.css"/>
-        <link rel="stylesheet"
-              href="unsemantic-grid-responsive-tablet.css">
-        <meta name="viewport" content="width=device-width,initial-scale=1,minimum-scale=1,maximum-scale=1"/>
-    </head>
+    header("Location:index.html");}
 
 
-    <body>
-    <div id="wrapper">
-        <div id="header">
-            <div id="logo">
-                <h1><strong><font size="20">GameShare RGU</font></strong></h1>
-            </div>
-            <div id="search">
-                <Form Name ="login" action="login.php" method="post">
-                    <? if(!isset($_SESSION['username'])){
-                        echo '
-                    <font size="-2"><label for="username">Username :</label><input id="username" name="username" type="text" size="-2"/><label for="Password">Password :</label><input id="password" name="password" type="password" size="-2"/></font><input class="form-submit" type="submit" value="Login" />
-                    <a id="register" href="registerDetails.php">Not a member? Register.</a>
-               ';}
-                    else{
-                        echo "Logged in as: ".$_SESSION['username'];
-                        echo ' <form name="logout" action="logout.php" method="post">
-                            <input id="logoutButton" type="submit" type="submit" value="Log Out">
-                            </form>';
-                    }
+error_reporting(-1);
 
+$dsn = "mysql:host=eu-cdbr-azure-north-d.cloudapp.net;dbname=db1510646_gameshare";
+$username = "b52b6c6935c6d2";
+$password = "26ebeed0";
+try {    $conn = new PDO($dsn, $username, $password);
+    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 
-                    ?>
-                </form>
-            </div>
-            <div id="menu">
-                <form action="results.php" method="post">
-                    <ul>
-                        <li><a href="index.html">Homepage</a></li>
-                        <li><a href="#"></a></li>
-                        <li><a href="#"></a></li>
-                        <li><a href="Search.php">Search</a></li>
-                        <li><input id="qsearch" name="qsearch" type="text" placeholder="I want to borrow..."/><input id="qsgo" type="submit"  value="Go"></li>
-                    </ul>
-                </form>
-                <br class="clearfix" />
-            </div>
-        </div>
-        <div id="page">
-            <div id="content">
+    $sql = "";
+    $query = "";
 
-                <main>
-                    <?php
-                    error_reporting(-1);
-
-                    $dsn = "mysql:host=eu-cdbr-azure-north-d.cloudapp.net;dbname=db1510646_gameshare";
-                    $username = "b52b6c6935c6d2";
-                    $password = "26ebeed0";
-                    try {
-                        $conn = new PDO($dsn, $username, $password);
-                        $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);;
-
-                        $query = "SELECT * FROM gamecollection
-                   WHERE Title = '" . $POST['Titles'] . "'";
-
-                        $conn->exec($query);
-
-                        $gameID = $row['GameID'];
-                        $id = $_SESSION['username'];
-                        $condition = $POST['conditionGame'];
-
-                        unset($query);
-                        $sql = "";
-
-
-                        $query = "INSERT INTO owns (gameID,studentID,game_condition) VALUES ('$gameID','$id','$condition')";
-
-                        $conn->exec($query);
-                        echo "New record created successfully";
+    $id=$_SESSION['username'];
+    $condition=$_POST['conditionGame'];
+    $gameID=$_POST['Titles'];
 
 
 
+   
 
-                    } catch (PDOException $e) {
-                        echo "Connection failed: " . $e->getMessage();
-                    }
-
-                    $conn = null;
-
-
-
-                    ?>
-
-                    <form>
-                        <form id="borrow"input type="submit" value="Back"
-                    </form>
+    $query = "INSERT INTO owns (gameID, studentID, game_condition) VALUES ('$gameID','$id','$condition')";
 
 
 
@@ -106,25 +32,22 @@ if (!isset($_SESSION['username'])) {
 
 
 
+    $conn->exec($query);
+    //$copy += 1;
 
 
 
-                </main>
+   /* $sql = "INSERT INTO owns (gameID, studentID, copyID, game_condition) VALUES ('$gameID','$id','$copy','$condition')";*/
 
 
-            </div>
-            <br class="clearfix" />
-        </div>
-        <div id="footer">
-            &copy; 2016. All rights reserved. Design by <strong>GROUP C</strong>.
-        </div>
-    </div>
-    </body>
+    //$conn->exec($sql);
+    echo "New record created successfully";
 
 
 
+} catch (PDOException $e) {
+    echo "Connection failed: " . $e->getMessage();
+}
 
-
-
-
-
+$conn = null;
+?>
