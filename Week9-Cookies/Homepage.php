0cfb5c3f5592fe50fadb3a3e0ebf969548aab8b4
collinsmarc <m.a.collins@rgu.ac.uk>
2016-04-11 09:51:08 +0100

    hh

--- Week9-Cookies/Homepage.php
+++ Week9-Cookies/Homepage.php
@@ -1,15 +1,18 @@
 <?php
 
+$access_level=$_COOKIE["access_level"];
 
 function displayAccessLevelInformation($access_level)
 {
     if ($access_level == "standarduser") {
         echo "<p> You are currently logged in as Standard User</p>";
     } elseif ($access_level == "root") {
-        echo "<p> Youa are logged in as root</p>";
+        echo "<p> You are logged in as root</p>";
         echo "<p> You now have access to admin features</p>";
     }
 }
+
+displayAccessLevelInformation($access_level);
 
 ?>
 
