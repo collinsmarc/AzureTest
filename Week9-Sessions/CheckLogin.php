370b45d2a019a760249963ce9e640c8b21c4994b
collinsmarc <m.a.collins@rgu.ac.uk>
2016-04-11 10:06:52 +0100

    hh

--- Week9-Sessions/CheckLogin.php
+++ Week9-Sessions/CheckLogin.php
@@ -6,14 +6,15 @@ session_start();
 $username=$_POST["username"];
 $password=$_POST["password"];
 
-$_SESSION['password']=$password;
+
 $_SESSION['username']=$username;
+$_SESSION['password']=$password;
 
 if (isset($_SESSION['username'])){
     set_session('access_level','standarduser');
     header("Location:Homepage.php");
 }
-else{
+elseif(!isset($_SESSION['username'])){
     echo "<p> INCORRECT </p>";
 
 }
