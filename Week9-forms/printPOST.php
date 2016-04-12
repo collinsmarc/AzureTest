a14afd1074e97988c12b16371b9e630768d6c194
collinsmarc <m.a.collins@rgu.ac.uk>
2016-04-12 09:40:05 +0100

    hh

--- Week9-forms/printPOST.php
+++ Week9-forms/printPOST.php
@@ -1,4 +1,19 @@
 <?php
 
 
-$title=$_POST[""]
+$title=$_POST["title"];
+$content=$_POST["content"];
+$animal=$_POST["animal"];
+
+echo "<article>
+<h1>{$title}</h1><br>
+<p>{$content}</p><br>
+<strong>{$animal}</strong><br>
+
+
+
+
+<article>";
+
+
+?>
