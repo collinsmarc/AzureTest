d269da663e7bae270653c3893f405e01539e2bb9
collinsmarc <m.a.collins@rgu.ac.uk>
2016-04-21 13:54:47 +0100

    hh

--- Group1703/sendEmail.php
+++ Group1703/sendEmail.php
@@ -1,6 +1,5 @@
 <?php
 require_once 'Swiftmailer/lib/swift_required.php';
-$name=$_SESSION['username'];
 
 $transport = Swift_SmtpTransport::newInstance('smtp.gmail.com', 465, "ssl")
     ->setUsername('gamesharergu@gmail.com')
@@ -10,7 +9,7 @@ $mailer = Swift_Mailer::newInstance($transport);
 $message = Swift_Message::newInstance('THIS IS THE SUBJECT')
     ->setFrom(array('gamesharergu@gmail.com' => 'YOUR NAME'))
     ->setTo(array('gamesharergu@gmail.com' => 'YOU'))
-    ->setBody('Hi ' );
+    ->setBody('This is the text of the mail send by Swift using SMTP transport.');
 //$attachment = Swift_Attachment::newInstance(file_get_contents('path/logo.png'), 'logo.png');  
 //$message->attach($attachment);
 $numSent = $mailer->send($message);
