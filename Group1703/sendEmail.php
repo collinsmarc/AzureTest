d81cd818512645dda992c21138597ecf29632f23
collinsmarc <m.a.collins@rgu.ac.uk>
2016-04-21 13:53:11 +0100

    hh

--- Group1703/sendEmail.php
+++ Group1703/sendEmail.php
@@ -10,20 +10,7 @@ $mailer = Swift_Mailer::newInstance($transport);
 $message = Swift_Message::newInstance('THIS IS THE SUBJECT')
     ->setFrom(array('gamesharergu@gmail.com' => 'YOUR NAME'))
     ->setTo(array('gamesharergu@gmail.com' => 'YOU'))
-    ->setBody('Hi '.$name.', <br>You have received a request to borrow one of your games. Please see the details below.
-                <table>
-                   <th>Title</th><th>Copy ID</th><th>BorrowerID</th>
-                    <td>'.$title = $row["Title"].'</td>
-                    <td>'.$id = $row["copyID"].'</td>
-                    <td>'.$_SESSION['username'].'</td>
-                </table>
-                <br>Please contact the user using there RGU email which is: '.$_SESSION['username'].'@rgu.ac.uk.
-                <br>If you agree upon a loan, then please remember to update the availability of said game on your profile.
-                <br>Regards,<br>   GameShare
-
-              '
-
-    );
+    ->setBody('Hi ' );
 //$attachment = Swift_Attachment::newInstance(file_get_contents('path/logo.png'), 'logo.png');  
 //$message->attach($attachment);
 $numSent = $mailer->send($message);
