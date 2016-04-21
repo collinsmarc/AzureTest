3ddb666032411ace0e9fe6f8b743385e92c50aee
collinsmarc <m.a.collins@rgu.ac.uk>
2016-04-21 14:00:09 +0100

    hh

--- Group1703/sendEmail.php
+++ Group1703/sendEmail.php
@@ -9,7 +9,10 @@ $mailer = Swift_Mailer::newInstance($transport);
 $message = Swift_Message::newInstance('THIS IS THE SUBJECT')
     ->setFrom(array('gamesharergu@gmail.com' => 'YOUR NAME'))
     ->setTo(array('gamesharergu@gmail.com' => 'YOU'))
-    ->setBody('This is the text of the mail send by Swift using SMTP transport.');
+    ->setBody('You Have Recieved A Borrow Request @Gameshare Please Check your Portal.
+
+    Thanks
+    Gameshare');
 //$attachment = Swift_Attachment::newInstance(file_get_contents('path/logo.png'), 'logo.png');  
 //$message->attach($attachment);
 $numSent = $mailer->send($message);
