ab993521fe9f2f96c51474f5456b49d006b686e7
collinsmarc <m.a.collins@rgu.ac.uk>
2016-04-21 13:52:04 +0100

    hh

--- Group1703/sendEmail.php
+++ Group1703/sendEmail.php
@@ -1,5 +1,6 @@
 <?php
 require_once 'Swiftmailer/lib/swift_required.php';
+$name=$_SESSION['username'];
 
 $transport = Swift_SmtpTransport::newInstance('smtp.gmail.com', 465, "ssl")
     ->setUsername('gamesharergu@gmail.com')
@@ -9,7 +10,20 @@ $mailer = Swift_Mailer::newInstance($transport);
 $message = Swift_Message::newInstance('THIS IS THE SUBJECT')
     ->setFrom(array('gamesharergu@gmail.com' => 'YOUR NAME'))
     ->setTo(array('gamesharergu@gmail.com' => 'YOU'))
-    ->setBody('This is the text of the mail send by Swift using SMTP transport.');
+    ->setBody('Hi '.$name.', <br>You have received a request to borrow one of your games. Please see the details below.
+                <table>
+                   <th>Title</th><th>Copy ID</th><th>BorrowerID</th>
+                    <td>'.$title = $row["Title"].'</td>
+                    <td>'.$id = $row["copyID"].'</td>
+                    <td>'.$_SESSION['username'].'</td>
+                </table>
+                <br>Please contact the user using there RGU email which is: '.$_SESSION['username'].'@rgu.ac.uk.
+                <br>If you agree upon a loan, then please remember to update the availability of said game on your profile.
+                <br>Regards,<br>   GameShare
+
+              '
+
+    );
 //$attachment = Swift_Attachment::newInstance(file_get_contents('path/logo.png'), 'logo.png');  
 //$message->attach($attachment);
 $numSent = $mailer->send($message);
