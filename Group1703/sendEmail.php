986f41999e023619ef2aa3acc520bf3be3ff275a
collinsmarc <m.a.collins@rgu.ac.uk>
2016-04-21 13:46:54 +0100

    hh

--- Group1703/sendEmail.php
+++ Group1703/sendEmail.php
@@ -1,21 +1,17 @@
-<?
+<?php
+require_once 'Swiftmailer/lib/swift_required.php';
 
+$transport = Swift_SmtpTransport::newInstance('smtp.gmail.com', 465, "ssl")
+    ->setUsername('gamesharergu@gmail.com')
+    ->setPassword('GamesharegroupC');
 
-        require_once 'Swiftmailer/lib/swift_required.php';
-
-        $transport = Swift_SmtpTransport::newInstance('smtp.office365.com', 587, 'tls')
-            ->setUsername('gamesharergu@outlook.com')
-            ->setPassword('GamesharegroupC');
-
-        $mailer = Swift_Mailer::newInstance($transport);
-        $message = Swift_Message::newInstance('THIS IS THE SUBJECT')
-            ->setFrom(array('gamesharergu@outlook.com' => 'YOUR NAME'))
-            ->setTo(array('gamesharergu@outlook.com' => 'YOU'))
-            ->setBody('This is the text of the mail send by Swift using SMTP transport.');
+$mailer = Swift_Mailer::newInstance($transport);
+$message = Swift_Message::newInstance('THIS IS THE SUBJECT')
+    ->setFrom(array('gamesharergu@gmail.com' => 'YOUR NAME'))
+    ->setTo(array('gamesharergu@gmail.com' => 'YOU'))
+    ->setBody('This is the text of the mail send by Swift using SMTP transport.');
 //$attachment = Swift_Attachment::newInstance(file_get_contents('path/logo.png'), 'logo.png');  
 //$message->attach($attachment);
-        $numSent = $mailer->send($message);
-        printf("Sent %d messages\n", $numSent);
-
-
+$numSent = $mailer->send($message);
+printf("Sent %d messages\n", $numSent);
 ?>
