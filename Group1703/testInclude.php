ea7f1cf1d2e2f4b5e0a697320e85cf328adc58c2
collinsmarc <m.a.collins@rgu.ac.uk>
2016-04-21 12:55:15 +0100

    hh

--- Group1703/testInclude.php
+++ Group1703/testInclude.php
@@ -1,7 +1,16 @@
 <?php
 header("Content-type: text/plain");
+require("sendgrid-php/sendgrid-php.php");
 require "config.php";
 
-echo "API key:".$apiKey;
+$sendgrid = new SendGrid($apiKey);
+$email    = new SendGrid\Email();
+
+$email->addTo("1510646@rgu.ac.uk")
+    ->setFrom("gamesharergu@gmail.com ")
+    ->setSubject("Sending with SendGrid is Fun")
+    ->setHtml("and easy to do anywhere, even with PHP");
+
+$sendgrid->send($email);
 ?>
 
