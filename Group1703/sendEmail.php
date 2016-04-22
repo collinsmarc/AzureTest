6ffdf4370e54cda10660d4f4bee0d5ee38c6638e
collinsmarc <m.a.collins@rgu.ac.uk>
2016-04-22 17:32:03 +0100

    Added files via upload

--- Group1703/sendEmail.php
+++ Group1703/sendEmail.php
@@ -2,25 +2,48 @@
 if(!isset($_SESSION['username'])){
     header("Location:home.php");
 }
-
 $user=($_SESSION['username']);
 
-require_once 'Swiftmailer/lib/swift_required.php';
+error_reporting(-1);
 
-$transport = Swift_SmtpTransport::newInstance('smtp.gmail.com', 465, "ssl")
-    ->setUsername('gameshareteamrgu@gmail.com')
-    ->setPassword('gameshare2016');
+$dsn = "mysql:host=eu-cdbr-azure-north-d.cloudapp.net;dbname=db1510646_gameshare";
+$username = "b52b6c6935c6d2";
+$password = "26ebeed0";
+try {
+    $conn = new PDO($dsn, $username, $password);
+    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 
-$mailer = Swift_Mailer::newInstance($transport);
-$message = Swift_Message::newInstance('Borrow Request')
-    ->setFrom(array('gameshareteamrgu@gmail.com' => 'GameShare Team'))
-    ->setTo(array('1510646@rgu.ac.uk' => 'Valued Gameshare Member'))
-    ->setBody('You Have Recieved A Borrow Request @Gameshare from '.$user.' Please Check your member portal.
+
+    $query = "SELECT * FROM members WHERE studentID LIKE '$user'";
+
+
+
+    $results = $conn->query($query);
+
+    $email=$row["email"];
+
+    require_once 'Swiftmailer/lib/swift_required.php';
+
+    $transport = Swift_SmtpTransport::newInstance('smtp.gmail.com', 465, "ssl")
+        ->setUsername('gameshareteamrgu@gmail.com')
+        ->setPassword('gameshare2016');
+
+    $mailer = Swift_Mailer::newInstance($transport);
+    $message = Swift_Message::newInstance('Borrow Request')
+        ->setFrom(array('gameshareteamrgu@gmail.com' => 'GameShare Team'))
+        ->setTo(array($email => 'Valued Gameshare Member'))
+        ->setBody('You Have Recieved A Borrow Request @Gameshare from ' . $user . ' Please Contact them at '.$email.' to arrange a swap!
 
     Thank You
     GameShare');
 //$attachment = Swift_Attachment::newInstance(file_get_contents('path/logo.png'), 'logo.png');  
 //$message->attach($attachment);
-$numSent = $mailer->send($message);
-printf("Sent %d messages\n", $numSent);
+    $numSent = $mailer->send($message);
+    printf("Sent %d messages\n", $numSent);
+
+}catch (PDOException $e) {
+    echo "Connection failed: " . $e->getMessage();
+}
+
+$conn = null;
 ?>
