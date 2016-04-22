a22c630e587d3b95fc7f22f7a5093c4693bf9947
collinsmarc <m.a.collins@rgu.ac.uk>
2016-04-22 17:54:45 +0100

    Added files via upload

--- Group1703/sendEmail.php
+++ Group1703/sendEmail.php
@@ -3,7 +3,7 @@ if(!isset($_SESSION['username'])){
     header("Location:home.php");
 }
 $user=($_SESSION['username']);
-$email= $_POST['confirmEmail'];
+$email= $_POST["confirmEmail"];
 
 
             require_once 'Swiftmailer/lib/swift_required.php';
@@ -15,7 +15,7 @@ $email= $_POST['confirmEmail'];
             $mailer = Swift_Mailer::newInstance($transport);
             $message = Swift_Message::newInstance('Borrow Request')
                 ->setFrom(array('gameshareteamrgu@gmail.com' => 'GameShare Team'))
-                ->setTo(array('1510646@rgu.ac.uk' => 'Valued Gameshare Member'))
+                ->setTo(array($email => 'Dear Valued Member'))
                 ->setBody('You Have Recieved A Borrow Request @Gameshare from ' . $user . ' Please Contact them at ' . $email . ' to arrange a swap!
 
     Thank You
