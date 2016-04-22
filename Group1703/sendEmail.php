1abe6557e1819c7227158dda258f6819628b9c32
collinsmarc <m.a.collins@rgu.ac.uk>
2016-04-22 17:44:19 +0100

    Added files via upload

--- Group1703/sendEmail.php
+++ Group1703/sendEmail.php
@@ -4,24 +4,55 @@ if(!isset($_SESSION['username'])){
 }
 $user=($_SESSION['username']);
 
-    require_once 'Swiftmailer/lib/swift_required.php';
 
-    $transport = Swift_SmtpTransport::newInstance('smtp.gmail.com', 465, "ssl")
-        ->setUsername('gameshareteamrgu@gmail.com')
-        ->setPassword('gameshare2016');
+error_reporting(-1);
 
-    $mailer = Swift_Mailer::newInstance($transport);
-    $message = Swift_Message::newInstance('Borrow Request')
-        ->setFrom(array('gameshareteamrgu@gmail.com' => 'GameShare Team'))
-        ->setTo(array($email => 'Valued Gameshare Member'))
-        ->setBody('You Have Recieved A Borrow Request @Gameshare from ' . $user . ' Please Contact them at '.$email.' to arrange a swap!
+$dsn = "mysql:host=eu-cdbr-azure-north-d.cloudapp.net;dbname=db1510646_gameshare";
+$username = "b52b6c6935c6d2";
+$password = "26ebeed0";
+try {
+    $conn = new PDO($dsn, $username, $password);
+    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+    $query = "SELECT * FROM members WHERE studentID LIKE '$user'";
+
+    $conn->exec($query);
+
+    if ($results->rowcount() == 0) {
+        echo "no member found <br />";
+    } else {
+
+        foreach ($results as $row) {
+            $user = $row["email"];
+
+
+            require_once 'Swiftmailer/lib/swift_required.php';
+
+            $transport = Swift_SmtpTransport::newInstance('smtp.gmail.com', 465, "ssl")
+                ->setUsername('gameshareteamrgu@gmail.com')
+                ->setPassword('gameshare2016');
+
+            $mailer = Swift_Mailer::newInstance($transport);
+            $message = Swift_Message::newInstance('Borrow Request')
+                ->setFrom(array('gameshareteamrgu@gmail.com' => 'GameShare Team'))
+                ->setTo(array($email => 'Valued Gameshare Member'))
+                ->setBody('You Have Recieved A Borrow Request @Gameshare from ' . $user . ' Please Contact them at ' . $email . ' to arrange a swap!
 
     Thank You
     GameShare');
 //$attachment = Swift_Attachment::newInstance(file_get_contents('path/logo.png'), 'logo.png');  
 //$message->attach($attachment);
-    $numSent = $mailer->send($message);
-    printf("Sent %d messages\n", $numSent);
+            $numSent = $mailer->send($message);
+            printf("Sent %d messages\n", $numSent);
+
+        }
+    }
+}
+     catch (PDOException $e) {
+        echo "Connection failed: " . $e->getMessage();
+    }
+
+    $conn = null;
 
 
 ?>
