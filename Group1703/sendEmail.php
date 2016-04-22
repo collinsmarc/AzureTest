64b3eaefe309805e518a428538e43465721087a9
collinsmarc <m.a.collins@rgu.ac.uk>
2016-04-22 17:44:41 +0100

    Added files via upload

--- Group1703/sendEmail.php
+++ Group1703/sendEmail.php
@@ -35,7 +35,7 @@ try {
             $mailer = Swift_Mailer::newInstance($transport);
             $message = Swift_Message::newInstance('Borrow Request')
                 ->setFrom(array('gameshareteamrgu@gmail.com' => 'GameShare Team'))
-                ->setTo(array($email => 'Valued Gameshare Member'))
+                ->setTo(array($user => 'Valued Gameshare Member'))
                 ->setBody('You Have Recieved A Borrow Request @Gameshare from ' . $user . ' Please Contact them at ' . $email . ' to arrange a swap!
 
     Thank You
