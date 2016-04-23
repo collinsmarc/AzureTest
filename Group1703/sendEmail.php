eb81b744697420f75e89b587711cb864c1cd9d81
collinsmarc <m.a.collins@rgu.ac.uk>
2016-04-23 12:18:58 +0100

    Added files via upload

--- Group1703/sendEmail.php
+++ Group1703/sendEmail.php
@@ -1,39 +1,8 @@
 <?php
 
-//header("Location:results.html");
 
-error_reporting(-1);
-
-$dsn = "mysql:host=eu-cdbr-azure-north-d.cloudapp.net;dbname=db1510646_gameshare";
-$username = "b52b6c6935c6d2";
-$password = "26ebeed0";
-try {
-    $conn = new PDO($dsn, $username, $password);
-    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-} catch (PDOException $e) {
-    echo "Connection failed: " . $e->getMessage();
-}
-
-
-$ownerGame=$_SESSION['username'];
-
-
-
-$sql = "SELECT gameCollection.Title, owns.copyID, members.firstName, members.lastName, members.email
-            FROM gameCollection INNER JOIN owns ON owns.GameID = gameCollection.gameID INNER JOIN members ON owns.studentID = members.studentID";
-try {
-    $results = $conn->query($sql);
-
-    if ($results->rowcount() == 0) {
-        echo "We're sorry, but the game you have requested is currently unavailable <br />";
-    } else {
-        $user = $row["email"];
         $email = $_POST["confirmEmail"];
-        $title = $row['Title'];
-        $id = $row['copyID'];
-        $first = $row['firstName'];
-        $lastname = $row['lastName'];
-        $email2 = $row['email'];
+
 
 
         require_once 'Swiftmailer/lib/swift_required.php';
@@ -45,8 +14,8 @@ try {
         $mailer = Swift_Mailer::newInstance($transport);
         $message = Swift_Message::newInstance('Borrow Request')
             ->setFrom(array('gameshareteamrgu@gmail.com' => 'GameShare Team'))
-            ->setTo(array($email2=> 'Dear Valued Member'))
-            ->setBody('You Have Recieved A Borrow Request @Gameshare. Please Contact them at ' . $email . ' to arrange a swap!
+            ->setTo(array('1510646@rgu.ac.uk'=> 'Dear Valued Member'))
+            ->setBody('You Have Recieved A Borrow Request @Gameshare. Please check your member portal to see the details, and if you are happy to proceed contact them at ' . $email . ' to arrange a swap!
 
     Thank You
     GameShare');
@@ -54,11 +23,9 @@ try {
 //$message->attach($attachment);
         $numSent = $mailer->send($message);
         header("Location:memberSite.php");
-    }
-    }catch (PDOException $e) {
-    echo "Query failed: " . $e->getMessage();
-}
-$conn = null;
+
+
+
 
 
 ?>
