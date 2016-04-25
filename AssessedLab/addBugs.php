52d7ee5a5bea0968633d52ef4e39ee60da67d4e9
collinsmarc <m.a.collins@rgu.ac.uk>
2016-04-25 09:42:18 +0100

    hh

---
+++ AssessedLab/addBugs.php
@@ -0,0 +1,107 @@
+<?
+include("connection.php");?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <link rel="stylesheet" href="bugsDesign.css" type="text/css"/>
+    <link href='https://fonts.googleapis.com/css?family=Oswald:700' rel='stylesheet' type='text/css'>
+    <link rel="stylesheet"
+          href="unsemantic-grid-responsive-tablet.css">
+    <meta name="viewport" content="width=device-width,initial-scale=1,minimum-scale=1,
+13 maximum-scale=1"/>
+
+    <title>BugTracker</title>
+</head>
+
+<body>
+<header>
+    <img id="logo" img src="logo.png">
+
+    <h1>Bug Tracker</h1>
+    <h2>Keeping track of all the pesky little bugs</h2>
+
+</header>
+<br><br>
+
+<main class="grid-container">
+    <aside class="grid-30">
+        <nav id="quick links">
+            <ul>
+                <li><a href="#">All Bug Items</a></li>
+                <li><a href="#">Android Bugs</a></li>
+                <li><a href="#">iOS Bugs</a></li>
+                <li><a href="#">Windows Bugs</a></li>
+                <li><a href="addBugs.html">Insert Bugs</a></li>
+            </ul>
+        </nav>
+    </aside>
+
+    <section class="grid-70">
+        <?php
+        if ($_SERVER['REQUEST_METHOD'] === 'GET') {
+        ?>
+            <form action="addBugs.php" method="post">
+
+                <Label>Bug Name</Label><input type="text" name="bugName" required><br><br>
+                <Label>Bug Summary</Label><br><textarea name="summary" cols="30" rows="5" required></textarea><br><br>
+                <Label>Bug Category</Label>
+                <select name="option">
+                    <option value="android">Android</option>
+                    <option value="ios">iOS</option>
+                    <option value="windows">Windows</option>
+                </select><br><br>
+
+
+
+                <input type="submit" value="Submit">
+            </form>
+
+
+            <?
+
+        }
+        elseif ($_SERVER['REQUEST_METHOD'] === 'POST') {
+        $name=$_POST["bugName"];
+        $summary=$_POST["summary"];
+        $category=$_POST["option"];
+
+
+
+        $sql="INSERT INTO bugs (bugName,BugSummary,BugCategory)
+VALUES ('$name','$summary','$category') ";
+
+        if (mysqli_query($db, $sql)) {
+            header("Location:showbugs.php");
+        }
+        } else {
+
+            header("Location:index.php");
+        }
+
+        $conn->close();
+        ?>
+
+
+
+
+
+
+
+
+    </section>
+
+
+</main>
+<footer>
+    <p>Designed by Marc Collins, 2016</p>
+
+
+</footer>
+
+
+
+
+</body>
+</html>
